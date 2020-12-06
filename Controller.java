3b3e1c49d8be7ff0881751c3a674fcb805f699f7
cd6629 <63489620+cd6629@users.noreply.github.com>
2020-12-06 01:56:26 -0500

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,103 @@
+//=============================================================================
+// PROGRAMMER: Dante Cook
+// PANTHER ID: 5686335
+//
+// CLASS: COP2210
+// SECTION: U04-U06
+// SEMESTER: Fall 2020
+// CLASSTIME: T/TH 5:00-6:15 pm
+//
+// Project: Project 3
+// DUE: 12/06/20
+//
+// CERTIFICATION:  I understand FIU’s academic policies, and I certify that this
+//                 work is my  own and that none of it is the work of any 
+//                 other person.
+//=============================================================================
+
+package app;
+
+import java.util.Random;
+import payment.CreditCard;
+import client.Person;
+import goods.Item;
+
+
+public class Controller {
+	public static void main(String[] args) throws InterruptedException{
+
+		Person john = new Person("John", "Doe", "1100 Brickell Ave", "Apt 102", "Miami", "Florida");
+
+		CreditCard masterCard = new CreditCard(john, "MasterCard", 2500.00);
+				
+		CreditCard ax = new CreditCard(john, "American Express", 5000.00);
+		
+		john.getCreditCards().add(masterCard);
+		john.getCreditCards().add(ax);
+		
+		Item cafeMocha = new Item("Food", "Cafe Mocha", 4.77);
+
+		Item gucciSlipper = new Item("Clothing", "Gucci Princetown", 2650.00);
+
+		Item coke = new Item("Food", "Coke", 1.99);
+
+		Item airlinesTicket = new Item("Travel", "MIA-SFO", 823.26);
+		
+		// john is buying cafeMocha using his MasterCard credit card
+		((CreditCard)john.getCreditCards().get(0)).makeCharge(cafeMocha);
+		
+		// john is buying gucciSlipper using his MasterCard credit card		
+		((CreditCard)john.getCreditCards().get(0)).makeCharge(gucciSlipper);
+		
+		// john is buying gucciSlipper using his American Express
+		((CreditCard)john.getCreditCards().get(1)).makeCharge(gucciSlipper);
+		
+		// john is running a transaction Report on his MasterCard
+		((CreditCard)john.getCreditCards().get(0)).transactionsReport();
+		
+		// john is running a transaction Report on his American Express
+		((CreditCard)john.getCreditCards().get(1)).transactionsReport();
+		
+		Random generator = new Random();
+				
+		for(int i=1; i<=7; i++){
+			Thread.sleep(generator.nextInt(1001));
+			if(i%3 == 0) {
+				((CreditCard)john.getCreditCards().get(0)).makeCharge(cafeMocha);
+			} else {
+				((CreditCard)john.getCreditCards().get(1)).makeCharge(cafeMocha);
+			}
+		}//end for
+		
+		for(int i=1; i<=5; i++){
+			Thread.sleep(generator.nextInt(1001));
+			if(i%2 == 0) {
+				((CreditCard)john.getCreditCards().get(0)).makeCharge(airlinesTicket);
+			} else {
+				((CreditCard)john.getCreditCards().get(1)).makeCharge(airlinesTicket);
+			}
+		}//end for
+		
+		for(int i=1; i<=10; i++){
+			Thread.sleep(generator.nextInt(1001));
+			int randomSelectCard = generator.nextInt(2);
+			if(randomSelectCard == 0) {
+				System.out.println("randomSelectCard: MasterCard");
+				((CreditCard)john.getCreditCards().get(0)).makeCharge(coke);
+			} else {
+				System.out.println("randomSelectCard: American Express");
+				((CreditCard)john.getCreditCards().get(1)).makeCharge(coke);
+			}
+			
+			
+		}//end for
+		
+		// john is running a transaction Report on his masterCard
+		((CreditCard)john.getCreditCards().get(0)).transactionsReport();
+		// john is running a transaction Report on his American Express
+		((CreditCard)john.getCreditCards().get(1)).transactionsReport();
+		// john is running displayInfo method
+		john.displayInfo();
+		
+	}
+}
